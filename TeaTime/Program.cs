df32786d52926f2e54d793c8c7bf3a092e4d5d16
EvanKent <evankent@live.com>
2015-04-08 01:25:21 -0700

    Real first commit, last was just making repo

---
+++ TeaTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeaTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class CommSetting
+        {
+            public static int CommPort = 0;
+            public static Int64 CommBaudRate = 0;
+            public static Int64 ComeTimeOut = 0;
+            public static GsmCommMain comm;
+
+            public CommSetting()
+            { 
+            
+            }
+        }
+    }
+}
